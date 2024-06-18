99233d1bb6f6ed4ac42e29f4dad4659a31cdefcc
Bishal Lamichhane <bishallc174@gmail.com>
2024-06-18 15:00:30 +0545

    [add] typeorm migration config and [fix] eslint prettier conflict

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProductModule } from './modules/product/product.module';
+import { ProductModule } from '@/modules/product/product.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { envConfig } from './configs/envConfig';
+import { TYPEORM_CONFIG } from '@/configs/typeorm';
 
 @Module({
-  imports: [
-    ProductModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: envConfig.DB_HOST,
-      port: envConfig.DB_PORT,
-      username: envConfig.DB_USER,
-      password: envConfig.DB_PASSWORD,
-      database: envConfig.DB_NAME,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
-      logging: true,
-    }),
-  ],
+  imports: [ProductModule, TypeOrmModule.forRoot(TYPEORM_CONFIG)],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/configs/typeorm.ts
+++ src/configs/typeorm.ts
@@ -1,14 +1,19 @@
-import { DataSource } from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { envConfig } from './envConfig';
+import { join } from 'path';
 
-export default new DataSource({
+export const TYPEORM_CONFIG = {
   type: 'postgres',
   host: envConfig.DB_HOST,
   port: envConfig.DB_PORT,
   username: envConfig.DB_USER,
   password: envConfig.DB_PASSWORD,
   database: envConfig.DB_NAME,
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  entities: [join(__dirname, '../**/*.entity{.ts,.js}')],
+  migrations: [join(__dirname, '../migrations/*{.ts,.js}')],
+  migrationsTableName: 'migrations',
   synchronize: true,
   logging: true,
-});
+} as unknown as DataSourceOptions;
+
+export default new DataSource(TYPEORM_CONFIG);
