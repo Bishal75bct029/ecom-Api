5eef12e41d6e884f2c34e1ea859a3523f827d971
Bishal Lamichhane <bishallc174@gmail.com>
2024-06-19 16:50:28 +0545

    fix import

--- src/modules/review/review.module.ts
+++ src/modules/review/review.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { ReviewService } from './review.service';
 import { ApiReviewController } from './api-review.controller';
 import { AdminReviewController } from './admin-review.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReviewEntity } from './entities/review.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ReviewEntity])],
   controllers: [ApiReviewController, AdminReviewController],
   providers: [ReviewService],
 })
