f9cb8890987afd72246f2f6c66e0872b98bf7970
Bishal Lamichhane <bishallc174@gmail.com>
2024-06-20 09:40:28 +0545

    manage configs

--- src/configs/envConfig.ts
+++ src/configs/envConfig.ts
@@ -10,4 +10,5 @@ export const envConfig = cleanEnv(process.env, {
   DB_USER: str(),
   DB_PASSWORD: str(),
   NODE_ENV: str({ default: 'local', choices: ['local', 'development', 'staging', 'production'] }),
+  PORT: port({ default: 3000 }),
 });
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AllExceptionFilter } from './common/filters/exception.filter';
+import { envConfig } from './configs/envConfig';
 
 (async () => {
   const app = await NestFactory.create(AppModule);
@@ -15,5 +16,5 @@ import { AllExceptionFilter } from './common/filters/exception.filter';
     }),
   );
   app.useGlobalFilters(new AllExceptionFilter(new Logger()));
-  await app.listen(3000);
+  await app.listen(envConfig.PORT, () => Logger.log(`Listening on port ${envConfig.PORT}`));
 })();
