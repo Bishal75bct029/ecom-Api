0dd890e18022f34b8105114a36e7d739acf79a23
Bishal Lamichhane <bishallc174@gmail.com>
2024-07-17 12:16:20 +0545

    run db migration

--- src/migrations/1721197709983-migration.ts
+++ src/migrations/1721197709983-migration.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class Migration1721129139706 implements MigrationInterface {
-  name = 'Migration1721129139706';
+export class Migration1721197709983 implements MigrationInterface {
+  name = 'Migration1721197709983';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`
