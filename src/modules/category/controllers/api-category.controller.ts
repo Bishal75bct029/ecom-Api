429d95a435ab7a360dfaab32003c1488f1ac411d
Bishal Lamichhane <bishallc174@gmail.com>
2024-07-18 15:52:27 +0545

    api get tree categories

--- src/modules/category/controllers/api-category.controller.ts
+++ src/modules/category/controllers/api-category.controller.ts
@@ -1,7 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { CategoryService } from '../services/category.service';
+import { Get } from '@nestjs/common';
 
 @Controller('api/categories')
 export class ApiCategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  @Get()
+  async categories() {
+    return this.categoryService.findTrees();
+  }
 }
