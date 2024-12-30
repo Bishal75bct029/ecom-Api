074177cfeb021af1533273d033d743b5c1f7cfd4
Bishal Lamichhane <bishallc174@gmail.com>
2024-12-30 16:03:37 +0545

    [feat]: integrate product schedule logic with bullmq

--- src/libs/queue/product/types.ts
+++ src/libs/queue/product/types.ts
@@ -1,15 +1,9 @@
 import { Job, Queue } from 'bullmq';
 
-export type ProductQueueJobType = 'SCHEDULE'; //add new job type
-
-interface ProductJobTypeMapping {
-  SCHEDULE: ProductQueueJobData; //add new job
-}
-
 // add other properties if needed
 export interface ProductQueueJobData {
   productId?: string;
+  scheduledDate?: Date;
 }
-
-export type ProductJob<T extends ProductQueueJobType = ProductQueueJobType> = Job<ProductJobTypeMapping[T], any, T>;
-export type ProductQueue = Queue<ProductJob, string, ProductQueueJobType, ProductQueueJobData>;
+export type ProductScheduleJob<T extends string = string> = Job<ProductQueueJobData, any, T>;
+export type ProductScheduleQueue = Queue<ProductScheduleJob<'SCHEDULE'>, string, 'SCHEDULE', ProductQueueJobData>;
