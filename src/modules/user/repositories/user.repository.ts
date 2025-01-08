54b8cfb820185dea5e1caf1088145c615052a324
Bishal Lamichhane <bishallc174@gmail.com>
2025-01-08 16:43:03 +0545

    refactor user repository

--- src/modules/user/repositories/user.repository.ts
+++ src/modules/user/repositories/user.repository.ts
@@ -3,14 +3,10 @@ import { Injectable } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { UserEntity } from '../entities';
 import { InjectRepository } from '@nestjs/typeorm';
-import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserRepository extends AbstractService<UserEntity> {
-  constructor(
-    @InjectRepository(UserEntity) private readonly itemRepository: Repository<UserEntity>,
-    private readonly jwtService: JwtService,
-  ) {
-    super(itemRepository);
+  constructor(@InjectRepository(UserEntity) private readonly productRepository: Repository<UserEntity>) {
+    super(productRepository);
   }
 }
